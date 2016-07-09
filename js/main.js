8fa4a831eafe08f47c8a9951f4eba96c37162a37
Josh-Weston <joshweston_02@icloud.com>
2016-07-09 13:34:10 -0300

    Storyboard Complete
    Storyboard is complete. Ready for review.

--- js/main.js
+++ js/main.js
@@ -203,8 +203,6 @@ function DataModel() {
             
         });
         
-        self.viewStoryBoard();
-        
     };
     
     /* Provides interactive explanation of the main story behind the visualization */
@@ -212,27 +210,38 @@ function DataModel() {
     /* Click to walk-through */
     self.viewStoryBoard = function(data) {
         
+        console.log(data);
+        //Start at scene 0;
+        var sceneNum = 0,
+            annotation = document.createElement('div');
+        
         //Clear any prior chart and cover.
         d3.select('#chart-area').html('');
+        
+        //Append annotation to chart-area for future formatting.
+        document.getElementById('chart-area').appendChild(annotation);
         
         //Find max across entire set to manually set y-axis.
         var max = d3.max(data, function(d) {
             return d.count;
-        })
+        });
         
+        //Only begin by showing subPrime
         var subPrime = dimple.filterData(data, 'creditCategory', 'Subprime'),
-            preCrash = [];
+            preCrashData = [],
+            pre2013 = [];
         
         for (var i = 0, len = subPrime.length; i < len; i++) {
-            
+
             if (subPrime[i]['quarter'] < '2009 Q1') {
-                preCrash.push(subPrime[i]);
+                preCrashData.push(subPrime[i]);
             }
-        
+
         }
         
+        
         var svg = dimple.newSvg('#chart-area', 1400, 700),
-            myChart = new dimple.chart(svg, preCrash);
+            myChart = new dimple.chart(svg, preCrashData);
         
         myChart.setBounds(60, 50, 1350, 600);
         
@@ -246,28 +255,261 @@ function DataModel() {
         y.overrideMax = max
         
         var dataSeries = myChart.addSeries('creditCategory', dimple.plot.line);
+        
+        //Scene for what happened before the crash
+        function preCrash() {
+            
+            dataSeries.afterDraw = function(){
+                
+                //Create first annotation
+                annotation.id = 'annotation';
+                annotation.innerHTML = 
+                'The financial crisis began in 2007, and was fueled by Subprime lending. ' +
+                'Before the eventual crash, Subprime loans were being approved at dangerously ' +
+                'high levels without proper assurances.';
+                
+                this.afterDraw = undefined;
+            };
+            
+            myChart.assignColor('Subprime', 'rgb(210, 107, 95)');
+            myChart.draw(1000);
+            
+        };
+        
+        
+        //Scene for what happened when the crash was at its worst.
+        function apex() {
+            
+            var y2 = myChart.addPctAxis('y', "Dummy");
+            y2.hidden = true;
+
+            var lineSeries = myChart.addSeries("FinancialCrisis", dimple.plot.area, [x, y2]);
+            lineSeries.lineWeight = 5;
+
+            lineSeries.data = [{
+                FinancialCrisis: "Financial Crisis",
+                Dummy: 1,
+                quarter: "2008 Q4"
+            }]
+            
+            lineSeries.afterDraw = function() {
+                
+                d3.select('#annotation').html('The financial crisis reached its apex at the end of 2008. ' + 
+                              'Many consumers lost their jobs and their homes, while financial ' +
+                              'institutions struggled to remain solvent.')
+                        .style('left', '1000px')
+                
+                //Clear the afterDraw
+                this.afterDraw = undefined;
+                
+            };
+
+            myChart.draw(1000);
+            
+            
+            //Remove reference line marker and change the line styling.
+            d3.select(lineSeries._markers['dimple-financial-crisis'][0][0]).style('display', 'none');
+            //Change color and opacity of dividing line.
+            d3.select(lineSeries.shapes[0][0]).style({
+                stroke: '#000',
+                opacity: 0.7,
+                cursor: 'pointer'
+            });
+
+            //Show manual tooltip
+            var lineEl = lineSeries.shapes[0][0];
+            lineEl.addEventListener('mouseover', function(event){
+
+                var x = event.clientX + 10,
+                    y = event.clientY;
+
+                //Remove any existing.
+                d3.selectAll('.manual-tooltip').remove();
+
+                //Add a new one.
+                var div = document.createElement('div');
+                div.className = 'manual-tooltip';
+                div.innerHTML = "The financial crisis at it's worst in 2008 Q4";
+
+                d3.select(div).style('top', y + 'px');
+                d3.select(div).style('left', x + 'px');
+
+                document.body.appendChild(div);
+
+            });
+
+            lineEl.addEventListener('mouseout', function(event){
+
+                //Remove any existing.
+                d3.selectAll('.manual-tooltip').remove();
+
+            });
+            
+        };
+        
+        
+        //Scene for what happened after the crash.
+        function postCrash() {
+            
+            for (var i = 0, len = subPrime.length; i < len; i++) {
+
+                if (subPrime[i]['quarter'] >= '2009 Q1') {
+                    preCrashData.push(subPrime[i]);
+                }
+
+            }
+
+            
+            dataSeries.afterDraw = function() {
+
+                d3.select('#annotation').html('In response to the crisis, Subprime loans fell out of favour and would not ' +
+                                              'return to pre-crisis levels.')
+                        .style({
+                            left: '900px',
+                            top: '475px'
+                        });
+                
+                this.afterDraw = undefined;
+
+            }
+            
+            myChart.draw(1000);
+                        
+        };
+        
+        
+        //Scene for rise in post-crash consumerism.
+        function consumerism() {
+            
+            //Populate remaining categories before skyrocketing debt levels.            
+            for (var i = 0, len = data.length; i < len; i++) {
+                
+                if (data[i]['quarter'] < '2013 Q4') {
+                    pre2013.push(data[i]);
+                }
+                
+            }
+            
+            //Add remaining credit categories to the dataset.
+            myChart.data = pre2013;
+            dataSeries = myChart.addSeries('creditCategory', dimple.plot.line);
+            
+            dataSeries.afterDraw = function() {
+                
+                d3.select('#annotation').html('However, in an effort to stimulte the economy, consumers were ' +
+                                              'encouraged to borrow more and spend more. The resutling increased consumerism ' +
+                                              'caused a steady rise in consumer debt.')
+                                        .style({
+                                            top: '200px',
+                                            left: '1100px'
+                                        });
+                
+                this.afterDraw = undefined;
+                
+            };
+            
+            myChart.assignColor('Acceptable', 'rgb(211, 150, 81)');
+            myChart.assignColor('Good', 'rgb(149, 185, 87)');
+            myChart.assignColor('Excellent', 'rgb(107, 148, 176)');
+            
+            myChart.draw(1000);
+            
+        };
+        
+        
+        //Scene for skyrocketing debt levels.
+        function skyrocket() {
+            
+            //Populate remaining quarters before skyrocketing debt levels.            
+            for (var i = 0, len = data.length; i < len; i++) {
+                
+                if (data[i]['quarter'] >= '2013 Q4') {
+                    pre2013.push(data[i]);
+                }
+                
+            }
+            
+            dataSeries.afterDraw = function() {
+                
+                d3.select('#annotation').html('Finally, in 2013, consumer debt skyrockets to levels well beyond the pre-crisis ' +
+                                              'numbers. Although Subprime credit approvals remain low, the staggering number of ' +
+                                              'loans approved for the remaining population could be foreboding another crisis.')
+                                        .style({
+                                            top: '70px',
+                                            left: '1050px'
+                                        });
+                
+                this.afterDraw = undefined;
+                
+            }
+            
+            //Add remaining entries.
+            myChart.draw(1000);
+        
+        }
+        
+        var scenes = [preCrash, apex, postCrash, consumerism, skyrocket];
+        //Add event-listener to chart.
+        d3.select('#chart-area').on('click', function() {
+            
+            if (sceneNum < scenes.length) {
+                
+                //Increment the scene and call the function.
+                scenes[sceneNum]();
+                sceneNum++;
+                
+            } else {
+                
+                //Remove event listener
+                d3.select('#chart-area').on('click', null);
+                
+                //Allow exploration
+                var buttons = document.getElementsByTagName('button');
+                
+                for (var i = 0, len = buttons.length; i < len; i++) {
+                    buttons[i].removeAttribute('disabled');
+                }
+                
+                d3.select('#annotation').style('display', 'none');
+                d3.selectAll('button').style('cursor', 'pointer');
+
+            }
+            
+        });
+
+                
+        
+        
+        
+        
+        
+        
+    
+
+        
+
+        
+
+        
+
+        
+
+        
         //var legend = myChart.addLegend(60, 10, 500, 20, "right");
-        myChart.assignColor('Subprime', 'rgb(210, 107, 95)');
         //myChart.assignColor('Acceptable', 'rgb(211, 150, 81)');
         //myChart.assignColor('Good', 'rgb(149, 185, 87)');
         //myChart.assignColor('Excellent', 'rgb(107, 148, 176)');
         
         //myChart.assignColor("Coolio", "red", "black", 1); We can set the opacity.
         
-        myChart.draw(1000);
         
-        
+        /*
         setTimeout(function() {
             
             
             
             
-            
-            
-            
-            
-       
-        /*Add 2008 crash reference line by creating a hidden y axis.*/
+        
         var y2 = myChart.addPctAxis('y', "Dummy");
         y2.hidden = true;
         
@@ -308,7 +550,6 @@ function DataModel() {
                 dataSeries.afterDraw = function() {
 
                 
-                
                 d3.select('#annotation').html('In response to the crisis, Subprime loans fell out of favour and would not ' +
                                               'return to pre-crisis levels.')
                         .style({
@@ -343,6 +584,7 @@ function DataModel() {
         //myChart.data = newDataset
         
         
+        */
         
         
         
@@ -355,21 +597,13 @@ function DataModel() {
         
         
         
-        
-        //Create first annotation
-        var div = document.createElement('div');
-        div.id = 'annotation';
-        div.innerHTML = 
-        'The financial crisis began in 2007, and was fueled by Subprime lending. ' +
-        'Before the eventual crash, Subprime loans were being approved at dangerously ' +
-        'high frequencies without proper assurances.';
+
         
         
-        //Needs to be set-up as absolute and appended to the chart-area.
-        document.getElementById('chart-area').appendChild(div);
+ 
         
-        div.style.top = top + 'px';
-        div.style.left = left + 'px';
+        //div.style.top = top + 'px';
+        //div.style.left = left + 'px';
 
         
         /*
